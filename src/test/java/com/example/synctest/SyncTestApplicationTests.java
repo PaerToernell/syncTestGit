12e5e48590891dc1e32ae3c5eea220eafdc814d7
PaerToernell <paer@ijob.se>
2022-07-21 15:03:07 +0200

    Initial commit

---
+++ src/test/java/com/example/synctest/SyncTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.synctest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SyncTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
